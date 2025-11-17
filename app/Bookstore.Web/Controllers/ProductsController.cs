2842e58ae8e8ba54d898b38289efa6d033cd8ef6
awsuser <root@ip-10-0-5-127.ec2.internal>
2025-11-17 13:02:43 +0000

    AWS Transform DB Mod commit for jobId f004d58f-06df-4b07-a580-138fb41f34de

--- app/Bookstore.Web/Controllers/ProductsController.cs
+++ app/Bookstore.Web/Controllers/ProductsController.cs
@@ -2,13 +2,14 @@ using Bookstore.Data;
 using Bookstore.Domain.Products;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
+
 
 namespace Bookstore.Web.Controllers
 {
